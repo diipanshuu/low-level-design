ba7a74bb96e1bc491c0f29eea7577ae0fa9de44f
diipanshuu <diipanshuu@gmail.com>
2024-06-07 23:22:28 +0530

    fix: Update in Strategy Design Pattern

--- Design-Patterns/strategy/Leetcode.java
+++ Design-Patterns/strategy/Leetcode.java
@@ -5,12 +5,30 @@ import java.util.Arrays;
 import static strategy.Approach.*;
 
 public class Leetcode {
+    private static Solution solution;
+
+    public Leetcode(Solution solution) {
+        this.solution = solution;
+    }
+
+    public void setSolution(Solution solution) {
+        this.solution = solution;
+    }
+
     public static void main(String[] args) {
+        Leetcode leetcode = new Leetcode(SolutionFactory.getSolution(BEST));
         int[] rainwater = new int[]{1, 2, 4, 6, 9, 3, 5};
-        
-        Solution solution = SolutionFactory.getSolution(BEST);
+
         assert solution != null;
         int[] result = solution.rainwaterTrapped(rainwater);
-        System.out.println(Arrays.toString(result));
+        System.out.println("Using Best approach " + Arrays.toString(result));
+        // Using Better Solution
+        leetcode.setSolution(new BetterSolution());
+        result = solution.rainwaterTrapped(rainwater);
+        System.out.println("Using Better approach " + Arrays.toString(result));
+        // Using BruteForce Solution
+        leetcode.setSolution(SolutionFactory.getSolution(BRUTE));
+        result = solution.rainwaterTrapped(rainwater);
+        System.out.println("Using Brute approach " + Arrays.toString(result));
     }
 }
